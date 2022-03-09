ae435e3eb710f7d1259df4b4df182ea61052c23e
jkinisnger <joy.kinsinger02@gmail.com>
2022-03-09 08:06:17 -0600

    added model, working on /beginning route of story

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var storyLine = require('../models/storyLine')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 
+router.get('/beginning', function(req, res, next) {
+  let story = storyLine.storyLines.find(peep => {
+    return peep.storyPart === parseInt(req.params.storyPart)
+  })
+  res.render('beginning', {story});
+  console.log(req.url)
+
+
+// router.get('/middle', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+// router.get('/end', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+});
 
 
 module.exports = router;
